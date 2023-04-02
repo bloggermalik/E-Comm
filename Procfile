a6eedfc5ed2704fa0ca12a7b50bbd92a2aab8d5a
bloggermalik <timepasstime123@gmail.com>
2023-04-02 16:24:19 +0530

    ok

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn customAuth.wsgi --log.file
--- customAuth/settings.py
+++ customAuth/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-rid8e*f&u(k8=6whqtrekof=@^!$yiu5%=a+(2it#y$+__981v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
