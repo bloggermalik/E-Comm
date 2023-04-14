aa232193adbf622a5a5f66ba7d2006388a840f1d
bloggermalik <timepasstime123@gmail.com>
2023-04-14 18:02:04 +0530

    ok

--- customAuth/settings.py
+++ customAuth/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-rid8e*f&u(k8=6whqtrekof=@^!$yiu5%=a+(2it#y$+__981v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['35.184.123.55']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
